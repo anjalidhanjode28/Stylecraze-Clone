e10701e60d1584a371554fe5e40d1c3d967b0526
anjalidhanjode28 <anjalidhanjode28@gmail.com>
2022-04-01 15:19:06 +0530

    my project stylecraze.com

---
+++ script/paymentData.js
@@ -0,0 +1,25 @@
+var paymentData = [
+    {
+      cardNum: "1234-5678-9123",
+      cvv: "123",
+      exp: "04/2025",
+      name: "Dishant Rana",
+      otp: "123",
+    },
+    {
+      cardNum: "9876-5432-1987",
+      cvv: "321",
+      exp: "05/2026",
+      name: "Diwanshi Rana",
+      otp: "321",
+    },
+    {
+      cardNum: "1234-5678-9123",
+      cvv: "123",
+      exp: "04/2022",
+      name: "Anjali Dhanjode",
+      otp: "000",
+    },
+  ];
+  localStorage.setItem("payData", JSON.stringify(paymentData));
+  
